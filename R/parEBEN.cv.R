28675650cc6ace6dc5ec17ab4486be027125c732
Colby Ford <colbytylerford@gmail.com>
2018-03-18 23:18:59 -0400

    Add doMPI functionality

--- R/parEBEN.cv.R
+++ R/parEBEN.cv.R
@@ -2,8 +2,11 @@ parEBEN.cv <- function (BASIS, Target, nFolds, Epis = "no", foldId = 0, parMetho
   if(parMethod == "doParallel"){
     result <- parEBEN.cv.doParallel(BASIS, Target, nFolds, Epis, foldId, prior)
     return(result)
+  } else if(parMethod == "doMPI"){
+    result <- parEBEN.cv.doMPI(BASIS, Target, nFolds, Epis, foldId, prior)
+    return(result)
   }
   else {
-    cat("Currently, only the `doParallel` function set for parallelization is available.")
+    cat("Currently, only the `doParallel` and `doMPI` function sets for parallelization are available.")
   }
 } 
